0680d0f8a0af7cf3d6df2cc753821916b26d84f9
beingpsikologi <adminbeing@gmail.com>
2026-08-11 22:13:24 +0700

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-const API_URL='https://script.google.com/macros/s/AKfycbxhg_eJiiFWXNKYdzrJGVpX16kDbwa3LNePMWNCGl4EVEjfD4IYs4znMsmtkitCoMmawA/exec';
+const API_URL="https://script.google.com/macros/s/AKfycbxhg_eJiiFWXNKYdzrJGVpX16kDbwa3LNePMWNCGl4EVEjfD4IYs4znMsmtkitCoMmawA/exec";
